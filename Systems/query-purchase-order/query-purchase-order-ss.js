573b4e53d11ea3411175c93ff55944d1c6816cf5
proglokan <92769441+proglokan@users.noreply.github.com>
2024-01-21 16:17:55 -0800

    new directory organization'

--- Systems/query-purchase-order/query-purchase-order-ss.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-// @subroutine {Helper} Void → open the query purchase order sidebar
-function queryPurchaseOrderMain() {
-    const html = HtmlService.createHtmlOutputFromFile('query-purchase-order-cs').setTitle('Purchase Order Query');
-    const ui = SpreadsheetApp.getUi();
-    ui.showSidebar(html);
-}
-//# sourceMappingURL=query-purchase-order-ss.js.map
