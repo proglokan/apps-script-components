1ea3bb9e28325a6c0ae563a5f40a3145166993ad
proglokan <92769441+proglokan@users.noreply.github.com>
2024-01-31 23:01:14 -0800

    Update user authentication logic

--- multi-purpose-applications/config-rendered-form/main/handle-form-submission.ts
+++
@@ -1,9 +0,0 @@
-import { fetchSheet, getHeaders, _Headers, getBody, Body } from "../../../global/global";
-type InputData = { [key: string]: string | boolean };
-function sendData(targetSpreadsheet: string | null, targetSheetID: number, inputData: InputData[]) {
-  const targetSheet = fetchSheet(targetSpreadsheet, targetSheetID);
-  const targetHeaders = getHeaders(targetSheet);
-  const targetSheetBody = getBody(targetSheet);
-  // . . .
-  return true;
-}
