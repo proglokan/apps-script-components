f2fd66851b1299cb460d373b94e535e2527fdd28
proglokan <92769441+proglokan@users.noreply.github.com>
2024-02-05 00:18:18 -0800

    Remove unused files and functions

---
+++ prototypes/profitzon-IO/profitzon-IO-ss.ts
@@ -0,0 +1,15 @@
+const dataStructure = {
+  name: 'Client Name',
+  workingCapitalLimit: 100,
+  balance: 0,
+  stores: [
+    {
+      id: 1,
+      inventory: [
+
+      ],
+      sales: null,
+      profitSplit: 0.5
+    }
+  ]
+}
