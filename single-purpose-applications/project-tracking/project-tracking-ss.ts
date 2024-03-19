96375e135f728c4a3269f1c23ff2b8cd47a0dbf7
Chance093 <chance.dev093@gmail.com>
2024-03-18 23:39:10 -0700

    refactor project-tracking

--- single-purpose-applications/project-tracking/project-tracking-ss.ts
+++ single-purpose-applications/project-tracking/project-tracking-ss.ts
@@ -1,27 +1,31 @@
+// ! This function is in global.ts
 function getSheet(): GoogleAppsScript.Spreadsheet.Sheet {
-    return SpreadsheetApp.getActive().getSheetByName('Project tracking') as GoogleAppsScript.Spreadsheet.Sheet;
+  return SpreadsheetApp.getActive().getSheetByName("Project tracking") as GoogleAppsScript.Spreadsheet.Sheet;
 }
 
+// TODO: Document this function (KANE)
 function handleInputs(data: string[]) {
-    const sheet: GoogleAppsScript.Spreadsheet.Sheet = getSheet();
-    const valRange: GoogleAppsScript.Spreadsheet.Range = sheet.getRange(2, 1, 1, 6);
-    const currentVals = valRange.getValues()[0] as string[];
-    const newVals: number[] = Array(6).fill(0);
-    for (let x = 0; x < data.length; ++x) {
-        const val: number = +currentVals[x];
-        const newVal: number = +data[x] + val;
-        newVals[x] = newVal;
-    }
-    valRange.setValues([newVals]);
-    const aggregateRange: GoogleAppsScript.Spreadsheet.Range = sheet.getRange(2, 7);
-    let aggregate: number = 0;
-    for (let x = 0; x < newVals.length; ++x) aggregate += newVals[x];
-    aggregateRange.setValue(aggregate);
+  const sheet: GoogleAppsScript.Spreadsheet.Sheet = getSheet();
+  const valRange: GoogleAppsScript.Spreadsheet.Range = sheet.getRange(2, 1, 1, 6);
+  const currentVals = valRange.getValues()[0] as string[];
+  const newVals: number[] = Array(6).fill(0);
+  for (let x = 0; x < data.length; ++x) {
+    const val: number = +currentVals[x];
+    const newVal: number = +data[x] + val;
+    newVals[x] = newVal;
+  }
+
+  valRange.setValues([newVals]);
+  const aggregateRange: GoogleAppsScript.Spreadsheet.Range = sheet.getRange(2, 7);
+  let aggregate: number = 0;
+  for (let x = 0; x < newVals.length; ++x) aggregate += newVals[x];
+  aggregateRange.setValue(aggregate);
 }
 
+// TODO: Document this function (KANE)
 function openForm() {
-    const template: GoogleAppsScript.HTML.HtmlTemplate = HtmlService.createTemplateFromFile('project-tracking-cs');
-    const html: GoogleAppsScript.HTML.HtmlOutput = template.evaluate().setWidth(500).setHeight(700);
-    const ui: GoogleAppsScript.Base.Ui = SpreadsheetApp.getUi();
-    ui.showModelessDialog(html, 'Project Tracking');
+  const template: GoogleAppsScript.HTML.HtmlTemplate = HtmlService.createTemplateFromFile("project-tracking-cs");
+  const html: GoogleAppsScript.HTML.HtmlOutput = template.evaluate().setWidth(500).setHeight(700);
+  const ui: GoogleAppsScript.Base.Ui = SpreadsheetApp.getUi();
+  ui.showModelessDialog(html, "Project Tracking");
 }
